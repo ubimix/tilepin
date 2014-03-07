532a99f5bec657bb571b5d2bb68f3a0b407600bb
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-03-07 17:59:03 +0100

    Committing test data for the example

--- examples/index.js
+++ examples/index.js
@@ -1,27 +1,16 @@
 (function(context) {
     $(function() {
-        var projectIdField = $('#projectPath');
-        var btn = $('#projectPathSubmit');
-
+        var tilesUrl = '/tiles/project/{z}/{x}/{y}.png';
         var center = [ 48.857487002645485, 2.3455810546875 ];
-        var zoom = 12;
-
+        var zoom = 2;
         var attribution = 'Map data &copy; '
                 + '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var map;
-        btn.on('click', function() {
-            var path = projectIdField.val();
-            if (map) {
-                map.remove();
-                map = null;
-            }
-            map = L.map('map').setView(center, zoom);
-            var tilesUrl = '/tiles/' + path + '/{z}/{x}/{y}.png';
-            L.tileLayer(tilesUrl, {
-                attribution : attribution,
-                maxZoom : 18
-            }).addTo(map);
-        })
+
+        var map = L.map('map').setView(center, zoom);
+        L.tileLayer(tilesUrl, {
+            attribution : attribution,
+            maxZoom : 6
+        }).addTo(map);
     });
 
 })(this);
